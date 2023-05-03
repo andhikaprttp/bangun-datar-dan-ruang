a536960782471816db0360448de52158a9734a5c
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-04 06:58:50 +0700

    update persegi.cpp

---
+++ persegi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sisi;
+    cout << "Masukkan panjang sisi persegi: ";
+    cin >> sisi;
+    int luas = sisi * sisi;
+    int keliling = 4 * sisi;
+    cout << "Luas persegi: " << luas << endl;
+    cout << "Keliling persegi: " << keliling << endl;
+    return 0;
+}
