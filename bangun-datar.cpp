7945a547337ad696cccefc5d8a4e02e426bce2ca
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-04 07:04:02 +0700

    update bangun-datar.cpp

--- bangun-datar.cpp
+++ bangun-datar.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
 using namespace std;
-int main() {
-  int sisi;
-  cout << "Masukkan panjang sisi persegi: ";
-  cin >> sisi;
-  int luas = sisi * sisi;
-  int keliling = 4 * sisi;
-  cout << "Luas persegi: " << luas << endl;
-  cout << "Keliling persegi: " << keliling << endl;
-    return 0;
-}
-
-#include <iostream>
-using namespace std;
 
 int main() {
     int pilihan;
